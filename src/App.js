fe799a4637fab7f4bd6096e19ea1f7c842a376b0
shaikhusama155 <shaikhusama155@gmail.com>
2023-09-07 16:20:15 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
+import StudentData from "./component/UserData";
+
+function App() {
+  return (
+    <>
+<StudentData/>
+    </>
+  );
+}
+
+export default App;
